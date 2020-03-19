e174538e3670fead0d0cb867d0e6a9d05a0efe71
SpokyOky <a4ottruba@gmail.com>
2020-03-19 23:17:48 +0400

    test

---
+++ SecuritySystemDatabaseImplement/Migrations/20200319184735_InitialCreate_2_test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecuritySystemDatabaseImplement.Migrations
+{
+    public partial class InitialCreate_2_test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
