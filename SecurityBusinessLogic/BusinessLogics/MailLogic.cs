2a1c2315d0887515a551101b77401801ddd022ec
SpokyOky <a4ottruba@gmail.com>
2020-06-10 00:19:21 +0400

    fix mail

--- SecurityBusinessLogic/BusinessLogics/MailLogic.cs
+++ SecurityBusinessLogic/BusinessLogics/MailLogic.cs
@@ -60,8 +60,7 @@ namespace SecurityBusinessLogic.BusinessLogics
                         objSmtpClient.EnableSsl = true;
                         objSmtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
                         objSmtpClient.Credentials = new NetworkCredential(mailLogin, mailPassword);
-
-                        await Task.Run(() => objSmtpClient.SendAsync(objMailMessage, null));
+                        objSmtpClient.Send(objMailMessage);
                     }
                     catch (Exception)
                     {
