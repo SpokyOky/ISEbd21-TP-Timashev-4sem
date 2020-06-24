ea604a90dfa9019716681b75985de3fc5a9b5691
SpokyOky <a4ottruba@gmail.com>
2020-06-24 20:25:45 +0400

    fixes

--- SecuritySystemDatabaseImplement/Migrations/20200624150026_release.Designer.cs
+++ SecuritySystemDatabaseImplement/Migrations/20200624150026_release.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SecuritySystemDatabaseImplement;
 
 namespace SecuritySystemDatabaseImplement.Migrations
 {
     [DbContext(typeof(SecuritySystemDatabase))]
-    partial class SecuritySystemDatabaseModelSnapshot : ModelSnapshot
+    [Migration("20200624150026_release")]
+    partial class release
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
